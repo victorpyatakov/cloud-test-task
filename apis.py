5f4d82b7436ae059dea9e5f5c30860589a988514
Victor Pyatakov <victor.pyatakov95@yandex.ru>
2022-03-20 21:07:30 +0000

    now id Full api

--- apis.py
+++ apis.py
@@ -1,26 +1,30 @@
-from flask import Blueprint
-from flask_restx import Api,Resource
-from utils import get_auth_token, get_vm, create_port, create_vm
-blueprint = Blueprint('api', __name__)
-api = Api(blueprint)
+from flask import Blueprint, request, json
+from flask_restx import Api,Resource, fields
+from utils import get_cloud_vm, create_vm, delete_vm
 
-@api.route('/auth')
-@api.doc()
-class Auth(Resource):
-    def get(self):
-        return get_auth_token()
+blueprint = Blueprint('api', __name__)
+api = Api(blueprint,version='1.0', title='API for Selectel Test Task')
+
+resource_fields = api.model('Resource', {
+    'is_preemptible': fields.Boolean,
+})
+
 
 @api.route('/vm')
 @api.doc()
 class VM(Resource):
     def get(self):
-        return get_vm()
+        return get_cloud_vm(), 200
     
+    @api.expect(resource_fields, validate=True)
     def post(self):
-        return create_vm()
+        data = json.loads(request.data)
+        return create_vm(is_preemptible=data["is_preemptible"]), 201
 
-@api.route('/port')
+@api.route('/vm/<cloud_id>')
 @api.doc()
-class Port(Resource):
-    def get(self):
-        return create_port()
+class InstanseVM(Resource):
+    @api.doc(params={'cloud_id': 'An ID in cloud'})
+    def delete(self, cloud_id):
+        return delete_vm(cloud_id)
+    
